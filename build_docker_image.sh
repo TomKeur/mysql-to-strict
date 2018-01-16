3474e9c5d35e3610c7d8dfc38d22932a1dd2b27a
Tom Keur <mail@tomkeur.net>
2018-01-16 21:58:47 +0100

    Small fix in build Docker build script.

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -5,7 +5,7 @@ CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -installsuffix cgo -o mysql-to
 version=`cat VERSION`
 echo "==> New version: $version"
 
-docker build -t mysql-to-strict:${version} -f Dockerfile .
+docker build -t tomkeur/mysql-to-strict:${version} -f Dockerfile .
 
 echo "==> Removing binary"
 rm -rf ./mysql-to-strict
